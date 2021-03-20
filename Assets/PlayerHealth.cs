d2686d373924499d66e2559e9ced8efeb6c2afee
ShivaOblivion <65827397+ShivaOblivion@users.noreply.github.com>
2021-03-20 22:55:33 +0100

    player Health

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHeart = 100;
+    public int currentHeart;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    public void TakeDamage(int damage)       
+    {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+
+    }
