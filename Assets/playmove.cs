f9cf00015968356c79099fe21312f16a20ef0fb4
ShivaOblivion <65827397+ShivaOblivion@users.noreply.github.com>
2021-03-20 16:19:43 +0100

    playmove 0.2

--- Assets/playmove.cs
+++ Assets/playmove.cs
@@ -1,43 +1,62 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class playmove : MonoBehaviour
 {
+    [Header("Movements")]
     public float moveSpeed;
-    public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
-    private bool isJumping = false;
+    [Header("Jump")]
+    public float CooldownJump = 5.0f;
+    private float TimeSinceJump = 6.0f;
+    public float distanceCheckGrounded = 0.5f;
     public float jumpForce;
+    public LayerMask groundLayer;
+    [Header("Refs")]
+    private Rigidbody2D rb;
+    private BoxCollider2D box2D;
 
-
+    private void Start()
+    {
+        box2D = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void FixedUpdate()
     {
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            isJumping = true;
-        }
-
         MovePlayer(horizontalMovement);
-
-
     }
+
+    private void Update()
+    {
+        TimeSinceJump += Time.deltaTime;
+        if (Input.GetButtonDown("Jump") && IsGrounded() && TimeSinceJump > CooldownJump)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            TimeSinceJump = 0.0f;
+        }
+    }
+
+
+    private bool IsGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distanceCheckGrounded, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
     void MovePlayer(float _horizontalMovement)
     {
-
-
-
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
 
-        if (isJumping)
-        {
-            rb.AddForce(new Vector2(0f, jumpForce));
-            isJumping = false;
-        }
     }
-}
 
+}
