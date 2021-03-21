fbadabf689b4f71ec6e1b98956b146aa98da9012
titilenne <80986359+titilenne@users.noreply.github.com>
2021-03-21 23:13:37 +0100

    ttt

--- Assets/PlayerCombat.cs
+++ Assets/PlayerCombat.cs
@@ -21,6 +21,7 @@ public class PlayerCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("isAttacking", false);
         if (Time.time >= nextAttackTime)
         {
             if (Input.GetKeyDown(KeyCode.P))
@@ -35,7 +36,7 @@ public class PlayerCombat : MonoBehaviour
     void Attack()
     {
         // Play an attack animation
-        animator.SetTrigger("Attack");
+        animator.SetBool("isAttacking", true);
 
         // Detect enemies in range of attack
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
@@ -45,6 +46,7 @@ public class PlayerCombat : MonoBehaviour
         {
             enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
         }
+        
     }
 
     private void OnDrawGizmosSelected()
