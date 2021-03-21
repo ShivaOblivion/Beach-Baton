4b23b4fdd429f27ad50ef40200d3bd29ea444bcd
An4tido2te0 <6-15871@saeinstitute.edu>
2021-03-21 17:29:27 +0100

    avancement foret avant mouv

---
+++ Assets/Bordel De Enzo/Background parallax/Parralax.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parralax : MonoBehaviour
+{
+    private float lenght, starpos;
+    public GameObject cam;
+    public float parralaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        starpos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parralaxEffect));
+        float dist = (cam.transform.position.x * parralaxEffect);
+        
+        transform.position = new Vector3(starpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > starpos + lenght) starpos += lenght;
+        else if (temp < starpos - lenght) starpos -= lenght;
+    
+    }
+}
